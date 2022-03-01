e4130ac7fbf900c151001cfd99410b99fb4bbc19
Memo <2415782224@qq.com>
2022-03-01 22:49:57 +0800

    SLF4j使用

--- SLF4J_STUDY/src/main/java/com/slf4j/SLF4JDemo.java
+++ SLF4J_STUDY/src/main/java/com/slf4j/SLF4JDemo.java
@@ -8,8 +8,8 @@ import org.slf4j.LoggerFactory;
  * @author: Rrow
  * @date: 2022/3/1 21:53
  */
-public class SLF4JTest01 {
-    Logger logger = LoggerFactory.getLogger(SLF4JTest01.class);
+public class SLF4JDemo {
+    Logger logger = LoggerFactory.getLogger(SLF4JDemo.class);
 
     /*
         入门案例
@@ -72,6 +72,44 @@ public class SLF4JTest01 {
             logger.info("aaa类获取出现了错误");
             logger.info("具体错误是",e);
         }
+    }
+
+    /*
+        NOP 还是和MAVEN顺序有关，谁在上方就先执行
+        Actual binding is of type [org.slf4j.helpers.NOPLoggerFactory]
+        slf4j-nop 可以禁止所有日志的打印，且必须在依赖顺序的最上方
+     */
+    @Test
+    public void Test05(){
+        try {
+            Class<?> aClass = Class.forName("aaa");
+        } catch (ClassNotFoundException e) {
+            logger.info("aaa类获取出现了错误");
+            logger.info("具体错误是",e);
+        }
+    }
+
+    /*
+        绑定Log4j
+        由于Log4j并没有比SLF4J之前出来，并没有遵循SLF4J规范，所以不能直接导入使用
+
+        如果想要使用，必须绑定适配器  slf4j-log4j12 ,再导入log4j的实现
+
+        log4j:WARN No appenders could be found for logger (com.slf4j.SLF4JTest01).
+        log4j:WARN Please initialize the log4j system properly.
+        log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.
+
+        使用了log4j框架，日志信息没有被打印出来，但是提示显示使用了log4j框架
+        提示没有使用  appenders 来进行加载，需要在日志执行前进行加载操作（初始化）
+
+     */
+    @Test
+    public void Test06(){
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
     }
 
 
