55bf42e5c072f8e9c13fe64c026be8c4a5e44a5c
Memo <2415782224@qq.com>
2023-06-14 20:41:25 +0800

    update

---
+++ SpringBootLog4j2DynamicLog/src/main/java/com/study/logger/LogApplication.java
@@ -0,0 +1,16 @@
+package com.study.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Rrow
+ * @date: 2023/6/14 20:32
+ * Description:
+ */
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class);
+    }
+}
